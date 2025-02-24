081f40bc84c26c70568622b77746a259f87a583f
Isobel Beasley <ibeasley@student.unimelb.edu.au>
2025-02-24 12:31:30 -0800

    Updating p differences testing script

--- code/gnomad_p_dist_diff.R
+++ code/gnomad_p_dist_diff.R
@@ -6,6 +6,93 @@
 
 # Next step to add to script:
 # Create further subcategories of differences 
+{
+  
+  afr_no_lof = seq(from = 0, to = 1, by = 0.02) #runif(n = 100000)
+  nfe_no_lof =  seq(from = 0, to = 1, by = 0.02)#runif(n = 100000)
+  
+  diff_df = expand.grid(afr_no_lof, nfe_no_lof)
+  
+  names(diff_df) = c("afr_no_lof", "nfe_no_lof")
+  
+  diff_df = diff_df %>% 
+    mutate(p_afr = 1 - sqrt(afr_no_lof),
+           p_nfe = 1 - sqrt(nfe_no_lof))
+  
+  # p_afr = 1 - sqrt(afr_no_lof)
+  # p_nfe = 1 - sqrt(nfe_no_lof)
+  
+  diff_df =  diff_df %>% 
+    # data.frame(afr_no_lof,
+    #                    nfe_no_lof,
+    #                    p_afr,
+    #                    p_nfe) %>% 
+    dplyr::mutate(no_lof_diff = abs(afr_no_lof - nfe_no_lof),
+                  p_diff = p_afr - p_nfe)
+  
+  # Filter points where |x - y| > 0.2
+  filtered_df <- diff_df %>% dplyr::filter(no_lof_diff > 0.1)
+  
+  # Split into two groups
+  above_df <- filtered_df %>% dplyr::filter(p_afr > p_nfe)  # Above y = x
+  below_df <- filtered_df %>% dplyr::filter(p_afr < p_nfe) # Below y = x
+  
+  above_df_line = above_df %>% 
+    dplyr::group_by(p_nfe) %>% 
+    slice_min(p_afr)
+  
+  below_df_line = below_df %>% 
+    dplyr::group_by(p_nfe) %>% 
+    slice_max(p_afr)
+  
+  poly2_above <- lm(p_afr ~ poly(p_nfe, 2, raw = TRUE), data= above_df_line)
+  poly2_below <- lm(p_afr ~ poly(p_nfe, 2, raw = TRUE), data= below_df_line)
+  
+  coef_above <- coef(poly2_above)
+  coef_below <- coef(poly2_below)
+  
+  # Print formulas
+  cat("Above y=x: p_afr =", coef_above[1], "+", coef_above[2], "* p_nfe +", coef_above[3], "* p_nfe^2\n")
+  cat("Below y=x: p_afr =", coef_below[1], "+", coef_below[2], "* p_nfe +", coef_below[3], "* p_nfe^2\n")
+  above_df_line$predicted <- predict(poly2_above, newdata = above_df_line)
+  below_df_line$predicted <- predict(poly2_below, newdata = below_df_line)
+  
+}  
+
+
+
+
+gnomad_lof$above_formula <- predict(poly2_above, newdata = gnomad_lof)
+gnomad_lof$below_formula <- predict(poly2_below, newdata = gnomad_lof)
+
+filtered_gnomad_lof = gnomad_lof %>% 
+  dplyr::filter(p_afr >= above_formula | p_afr <= below_formula)
+
+filtered_gnomad_lof %>% 
+  ggplot(aes(x = p_nfe, y = p_afr)) + 
+  geom_abline(slope =1, intercept = 0, color = "firebrick")+  
+  geom_line(data = above_df_line, aes(x = p_nfe, y = predicted), color = "blue", size = 0.5, linetype = "dashed") +
+  geom_line(data = below_df_line, aes(x = p_nfe, y = predicted), color = "blue", size = 0.5, linetype = "dashed") +
+  geom_point(alpha = 0.25) + 
+  theme_bw() +
+  labs(title = "AFR vs NFE (p)") 
+
+gnomad_lof %>% 
+  ggplot(aes(x = p_nfe, y = p_fin)) + 
+  geom_abline(slope =1, intercept = 0, color = "firebrick")+  
+  geom_line(data = above_df_line, aes(x = p_nfe, y = predicted), color = "blue", size = 0.5, linetype = "dashed") +
+  geom_line(data = below_df_line, aes(x = p_nfe, y = predicted), color = "blue", size = 0.5, linetype = "dashed") +
+  geom_point(alpha = 0.25) + 
+  theme_bw() +
+  labs(title = "FIN vs NFE (p)")
+
+
+gnomad_lof %>% 
+  ggplot(aes(x = p_nfe, y = p_amr)) + 
+  geom_abline(slope =1, intercept = 0, color = "firebrick")+  
+  geom_point() + 
+  theme_bw() +
+  labs(title = "AMR vs NFE (p)")
 
 
 
